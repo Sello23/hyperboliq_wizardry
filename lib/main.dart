3ef766415f542349f4da7f74669723cf6d045812
SelloN <102992967+Sello23@users.noreply.github.com>
2024-08-22 14:59:23 +0200

    Refactor Search

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hyperboliq/widgets/search_text_bar.dart';
 import 'models/spell_model.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'widgets/search_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,47 +12,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const SpellSearchScreen(),
+      title: 'Spell Search',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: _SpellSearchState(),
     );
   }
 }
 
-class SpellSearchScreen extends StatefulWidget {
-  const SpellSearchScreen({super.key});
-
-  @override
-  SpellSearchState createState() => SpellSearchState();
-}
-
-class SpellSearchState extends State<SpellSearchScreen> {
+class _SpellSearchState extends StatelessWidget {
   final List<Spell> _spells = [];
-  List<Spell> _searchResults = [];
-
-  Future<List<Spell>> _fetchSpells(String query) async {
-    final response = await http.get(Uri.parse('https://wizard-world-api.herokuapp.com/Spells?name=$query'));
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-      return data.map((spellJson) => Spell.fromJson(spellJson)).toList();
-    } else {
-      throw Exception('Failed to load spells');
-    }
-  }
-
-  void _onSearchQueryChanged(String query) async {
-    if (query.isNotEmpty) {
-      final results = await _fetchSpells(query);
-      setState(() {
-        _searchResults = results;
-      });
-    } else {
-      setState(() {
-        _searchResults.clear();
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,16 +33,13 @@ class SpellSearchState extends State<SpellSearchScreen> {
       body: Column(
         children: [
           SearchTextBar(
-            onQueryChanged: _onSearchQueryChanged,
             onSelected: (Spell? spell) {
+              _spells.clear();
               if (spell != null) {
-                setState(() {
-                  _spells.add(spell);
-                  _searchResults.clear();
-                });
+                _spells.add(spell);
               }
+              (context as Element).markNeedsBuild();
             },
-            searchResults: _searchResults,
           ),
           Expanded(
             child: ListView.builder(
